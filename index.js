444bdd5ae1d1bef13ccf95d45085d7d942ef7597
cuyopeludo <120688547+cuyopeludo@users.noreply.github.com>
2023-09-20 18:54:11 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+function septup(){
+    video=createCapture(VIDEO); 
+    video.size(987,789)   
+    canvas=createCanvas(780,543);
+canvas.position(897,968);
+
+    
+}
+
+function preload(){
+
+
+
+
+
+
+
+}
+
+function draw(){
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
